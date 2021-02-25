15061d5c189fe288a2ed784dc5f374d3647add1d
Adam Freiberg <xfreib00@stud.fit.vutbr.cz>
2021-02-25 23:30:30 +0100

    Add basic CMake structure

--- cmake/gram.cmake
+++ cmake/gram.cmake
@@ -1,11 +1,14 @@
 include(FetchContent)
 
+set(GRAM_BUILD_TESTS ON CACHE INTERNAL "Build tests for Gram lib")
+
 FetchContent_Declare(
     gram
     GIT_REPOSITORY https://github.com/jansvoboda11/gram.git
-    GIT_TAG origin/master
+    GIT_TAG     origin/main
+    GIT_CONFIG  advice.detachedHead=false
 )
 
 FetchContent_MakeAvailable(gram)
 
-set(gram_INCLUDE_DIR "${GEHash_SOURCE_DIR}/include/gram/")
+set(gram_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/include/gram/include/")
