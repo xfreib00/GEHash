1605ea650f9f8e0c641d70ece92a3f4646b49772
Adam Freiberg <xfreib00@stud.fit.vutbr.cz>
2021-03-25 20:17:12 +0100

    Set GELogger class output as JSON
    Use Nlohmann JSON as JSON parser used for logger output.

--- include/logger.h
+++ include/logger.h
@@ -9,11 +9,16 @@
 
 #include <stdexcept>
 #include <fstream>
+#include <iostream>
+#include <iomanip>
 #include <gram/population/Population.h>
 #include <gram/util/logger/Logger.h>
+#include <gram/language/mapper/ContextFreeMapper.h>
 #include <nlohmann/json.hpp>
 
 using namespace gram;
+using namespace std;
+using json = nlohmann::json;
 
 /**
  * @brief Logger class for GEHash grammatical evolution.
@@ -30,20 +35,34 @@ public:
 	/**
 	 * @brief Parameterized constructor of Logger class.
      * @param [in] path Reference to path to output file.
+	 * @param [in] logMapper Unique pointer to initilized ContextFreeMapper
+	 * object used for mapping genotype to phenotype inside GELogger class.
 	 */
-	GELogger(const std::string& path);
+	GELogger(const string& path, unique_ptr<ContextFreeMapper> logMapper);
 	
 	/**
 	 *	@brief Log progress of current evolution run.
-	 *	@param [in] population Reference population object.
+	 *	@param [in] population Reference to population object.
 	 */
 	void logProgress(const Population& population);
 
 	/**
 	 * @brief Log result of evolution run.
-	 * @param [in] population Reference population object.
+	 * @param [in] population Reference to population object.
 	 */
 	void logResult(const Population& population);
+
+	/**
+	 * @brief Getter of debug flag.
+	 * @return Current value of debug flag.
+	 */
+	bool getDebug(void);
+
+	/**
+	 * @brief Setter of debug flag.
+	 * @param [in] val New value of debug flag.
+	 */
+	void setDebug(bool val);
 
 	/**
 	 * @brief Logger class destructor.
@@ -55,7 +74,24 @@ private:
 	/**
 	 * @brief Fstream variable for Logger output file. 
 	 */
-	std::fstream out;
+	fstream out;
+
+	/**
+	 * @brief Nlohmann::json object.
+	 */
+	json j_out;
+
+	/**
+	 * @brief Unique poiter to ContextFreeMapper used to generate phenotype.
+	 */
+	unique_ptr<ContextFreeMapper> mapper;
+
+	/**
+	 * @brief Debug flag.
+	 * @details This flag is used to enable mapping genotype to phenotype in
+	 * GELogger::logProgress function that results in longer execution time.
+	 */
+	bool debug = false;
 
 };
 #endif
--- src/logger.cpp
+++ src/logger.cpp
@@ -6,25 +6,66 @@
 
 #include "logger.h"
 
-GELogger::GELogger(const std::string& path)
+GELogger::GELogger(const std::string& path, unique_ptr<ContextFreeMapper> logMapper)
 {
+    /* check if path is not empty */
     if (path.empty()){
-        throw std::invalid_argument("Missing Logger path");
+        throw std::invalid_argument("Missing GElogger path");
     }
-    out.open(path);
+
+    /* try to open file at given path */
+    try
+    {
+        mapper = move(logMapper);
+        out.open(path);
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
 }
 
 void GELogger::logProgress(const Population& population)
 {
-    
+    json j;
+    /* log best individual in current generation */
+    j["status"] = "progress";
+    j["gen"] = population.generationNumber();
+    j["fitness"] = population.individualWithLowestFitness().fitness();
+    if (debug){
+        j["phenotype"]["code"] = population.individualWithLowestFitness().serialize(*mapper);
+    }
+    j_out.push_back(j);
 }
 
 void GELogger::logResult(const Population& population)
 {
+    json j;
 
+    /* log best individual in final generation */
+    j["status"] = "result";
+    j["gen"] = population.generationNumber();
+    j["fitness"] = population.individualWithLowestFitness().fitness();
+    j["phenotype"]["code"] = population.individualWithLowestFitness().serialize(*mapper);
+
+    j_out.push_back(j);
+
+    /* write logger output to file */
+    out << setw(4) << j_out << endl;
+}
+
+bool GELogger::getDebug(void)
+{
+    return debug;
+}
+
+void GELogger::setDebug(bool val)
+{
+    debug = val;
 }
 
 GELogger::~GELogger()
 {
+    /* close file */
     out.close();
 }
