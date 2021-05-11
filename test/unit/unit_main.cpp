57e696a09114fbfdfb0c33300b497d87c1725a7c
Adam Freiberg <xfreib00@stud.fit.vutbr.cz>
2021-05-11 17:22:06 +0200

    Cmake comments and add catch2 testing
    New comments for individual CMakeLists.txt files and add test directory
    for unit testing.

---
+++ test/unit/unit_main.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file unit_main.cpp
+ * @author your name (you@domain.com)
+ * @brief Main testing file for unit tests
+ * @version 0.1
+ * @date 2021-05-10
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+#define CATCH_CONFIG_MAIN
+
+#include <catch.hpp>
+
+//
